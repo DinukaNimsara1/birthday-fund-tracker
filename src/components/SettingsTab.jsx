4b5a6cb5a368bcd020615b2daf1fadfc9a290fc1
dinukan <dinukan@codegen.net>
2026-01-16 11:37:06 +0530

    feat: remove dupliacte

--- src/components/SettingsTab.jsx
+++ src/components/SettingsTab.jsx
@@ -7,8 +7,6 @@ export default function SettingsTab({
   onUpdateMonthlyAmount,
   onAddMember,
   onRemoveMember,
-  onAddMember,
-  onRemoveMember,
   onUpdateMemberBirthday,
   managerPassword,
   onUpdateManagerPassword
